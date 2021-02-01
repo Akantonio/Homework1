ccd4f9a404e0fe7dda4036b0c4017eaf931893ca
Akantonio <adrianvantonio@yahoo.com>
2021-01-31 20:55:00 -0900

    added a function for testing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include "StopWatch.h"
 
+void testIng(){
+    for(int i=0; i<1000; ++i){
+        std::cout<< i << std::endl;
+    }
+}
+
 int main() {
     StopWatch theClock;
+    theClock.start();
+    testIng();
+    theClock.stop();
 
     return 0;
 }
