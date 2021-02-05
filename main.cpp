7f60f3ba7db62a4257fcd89ec0b360432e89c480
Akantonio <adrianvantonio@yahoo.com>
2021-02-05 14:37:05 -0900

    added timer inside the binary and search functions

--- main.cpp
+++ main.cpp
@@ -17,15 +17,28 @@ int randomInt(int minimum,int maximum){
     return dis(gen);
 }
 
-void search(std::vector<int> & data){
-    std::vector<int> parameter{3,4,5,6}; //search parameters
+void search(std::vector<int> & data,StopWatch & clockT){
+    std::vector<int> parameter{3,4,5}; //search parameters
     std::vector<int>::iterator intPoint;
-
+    clockT.start();
     intPoint= std::search(data.begin(), data.end(), parameter.begin(), parameter.end());
+    clockT.stop();
+    clockT.readTime(1);
     if(intPoint != data.end()){
         std::cout<<"Parameter is in the Data "<<(intPoint - data.begin())<<"\n";
     }else{
-        std::cout<<" Parameter is not present in Data\n";
+        std::cout<<"Parameter is not present in Data\n";
+    }
+}
+
+void binarySearch(std::vector<int> & data,StopWatch & clockT){
+    clockT.start();
+    if(std::binary_search(data.begin(),data.end(),3)){
+        clockT.stop();
+        std::cout<<"Found a 3! \n";
+    } else{
+        clockT.stop();
+        std::cout<<"Did not Find a 3!\n";
     }
 }
 
@@ -33,16 +46,24 @@ int main() {
     //Time It I
     StopWatch theClock;
 
+    int capacity=100;
     std::vector<int> randomVector;
-    randomVector.reserve(10);
-    for(int i=0;i<10;++i){
+    randomVector.reserve(capacity);
+    for(int i=0; i < capacity; ++i){
         randomVector.push_back(randomInt(1,10));
     }
 
-    theClock.start();
-    search(randomVector);
-    theClock.stop();
-    theClock.readTime(1);
+    //using std::search
+    std::cout<<"-Search-"<<std::endl;
+
+    search(randomVector,theClock);
+
+
+    //using std::binary_search
+    std::cout<<"-Binary Search-"<<std::endl;
+
+    binarySearch(randomVector,theClock);
+
 
     //Gutenberg
     std::string bookLine;
@@ -51,12 +72,14 @@ int main() {
     if(!fInput){
         std::cout<<"Could not read File!" << std::endl;
     }
+    std::cout<<" -String- "<<std::endl;
     theClock.start();
     while(std::getline(fInput,bookLine)){
-        //std::cout << bookLine <<std::endl;
+        std::cout << bookLine <<std::endl;
     }
     theClock.stop();
-    theClock.readTime(0);
+    theClock.readTime(1);
+
 
     return 0;
 }
