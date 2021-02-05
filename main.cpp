303a40cd63114b39c213a79bc948492770e586f6
Akantonio <adrianvantonio@yahoo.com>
2021-02-05 07:43:02 -0900

    added example random code

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 
 #include "StopWatch.h"
+#include <random>
 
 void testIng(){
     for(int i=0; i<1000; ++i){
         std::cout<< i << std::endl;
     }
+}
+
+void randomT(){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 6);
+
+    for (int n=0; n<10; ++n)
+
+        std::cout << dis(gen) << ' ';
+    std::cout << '\n';
+}
+
 }
 
 int main() {
@@ -14,6 +28,9 @@ int main() {
     theClock.stop();
     theClock.readTime(0);
     theClock.readTime(1);
+
+    theClock.start();
+    randomT();
 
     return 0;
 }
